19b391bbeb47a54d606c7cd1abf4fe3798df7071
Prophecy67 <Prophecy67@gmail.com>
2019-09-16 18:12:26 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+module "eks" {
+  source                          = "terraform-aws-modules/eks/aws"
+  cluster_name                    = "my-cluster"
+  cluster_endpoint_private_access = "true"
+
+  subnets = [
+    data.terraform_remote_state.vpc.outputs.aws_subnet_jenkins-subnet-eu-west-1a,
+    data.terraform_remote_state.vpc.outputs.aws_subnet_jenkins-subnet-eu-west-1b,
+  ]
+
+  vpc_id = data.terraform_remote_state.vpc.outputs.aws_vpc_vpc-jenkins
+
+  worker_groups = [
+    {
+      instance_type       = "m4.large"
+      asg_max_size        = 5
+      propagate_at_launch = true
+    },
+  ]
+
+  tags = {
+    environment = "test"
+  }
+}
